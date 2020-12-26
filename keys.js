ecb5bd251c06bd460a274d573fe8d4fc5b2a6a34
Mone E <mone.eunique12@gmail.com>
2020-12-25 21:20:44 -0800

    fixed client route

--- keys.js
+++ keys.js
@@ -2,6 +2,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: '79e0f4b731c54797b57f7366c09a88c2',
+  secret: 'ad1a2543c9114d2a8cbd7bcc8aaad274'
 };
